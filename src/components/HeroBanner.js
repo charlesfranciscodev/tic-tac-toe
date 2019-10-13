5f8b36c5b2f571d0405f6a1ff3354fedfa786f86
Charles-Antoine Francisco <charlantfr@gmail.com>
2019-10-13 12:36:48 -0400

    Bulma Setup

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HeroBanner() {
+  return (
+    <section className="hero is-success is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            Tic Tac Toe - Free Code Camp
+          </h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroBanner;
